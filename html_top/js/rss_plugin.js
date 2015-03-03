9b37e1decde680dceb281ced0fd76d683934265f
trd.ronald <trd.ronald@gmail.com>
2015-03-03 18:03:26 +0800

    create layout for rss feed

---
+++ html_top/js/rss_plugin.js
@@ -0,0 +1,24 @@
+(function($){
+	
+	$.fn.newFeed = function(){
+		var elem = $("<div>");
+		var title = $("<p>");
+		var content = $("<p>");
+		
+		elem.attr("class","cd-rss-field-item");
+		title.attr("class","cd-rss-field-item-date");
+		content.attr("class","cd-rss-field-item-feed");
+		
+		title.html("2015/03/03");
+		content.html("あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ");
+		
+		elem.append(title);
+		elem.append(content);
+		console.log(elem);
+		$(".cd-rss-field").append(elem);		
+	};
+
+
+
+
+})(jQuery);
