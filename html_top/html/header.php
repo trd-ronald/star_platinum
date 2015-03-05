fb7c9adabfbc833ab89f7db9a5a74f008743cc41
trd.ronald <trd.ronald@gmail.com>
2015-03-05 12:12:55 +0800

    -took out echo

--- html_top/html/header.php
+++ html_top/html/header.php
@@ -1,63 +1,58 @@
-<?php
-echo '
-		<!DOCTYPE HTML>
-		<html lang="en" class="no-js">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width,height=device-height,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
-		
-		    <link rel="stylesheet" type="text/css" href="font-awesome-4.2.0/css/font-awesome.css">
-			<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
-			<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
-			<link rel="stylesheet" href="css/drawer.css"> <!-- Resource style -->
-			<link rel="stylesheet" href="css/venovox.css">
-			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-			<script type="text/javascript" src="js/venobox.min.js"></script>
-			<link href="http://fonts.googleapis.com/css?family=Roboto:400,700,300" rel="stylesheet" type="text/css">
-			<title>Star Platinum</title>
-		</head>
-		<body>
-			<div class="navigation">
-			<ul>
-			<li class="nav-item"><a href="#" class="menu-header"></a></li>
-			<li class="nav-item">
-		 		<a href="#" class="menu-links">>　会社概要</a>
-				<ul class="nav-item-sub">
-					<li class="menu-links"><a href="#">&nbsp;&nbsp;1</a></li>
-					<li class="menu-links"><a href="#">&nbsp;&nbsp;2</a></li>
-					<li class="menu-links"><a href="#">&nbsp;&nbsp;3</a></li>
-				</ul>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="menu-links">>　制作実績</a>
-				<ul class="nav-item-sub">
-					<li class="menu-links"><a href="#">&nbsp;&nbsp;WEB関連</a></li>
-					<li class="menu-links"><a href="#">&nbsp;&nbsp;広告／印刷</a></li>
-					<li class="menu-links"><a href="#">&nbsp;&nbsp;教育／育成</a></li>
-					<li class="menu-links"><a href="#">&nbsp;&nbsp;イベント企画</a></li>
-					<li class="menu-links"><a href="#">&nbsp;&nbsp;マネージメント</a></li>
-				</ul>
-			</li>
-			<li class="nav-item"><a href="#" class="menu-links">>　お問い合わせ</a></li>
-			<li class="nav-item last-item"><a href="#" class="menu-copyright">© STAR PLATINUM INC. ALL RIGHTS RESERVED</a></li>
-			</ul>
-			<div class="contact">
-				<img src="img/img1/contact_us.png" alt="コンタクト">
-				<p>〒810-0041<br>
-					 福岡県福岡市中央区大名2丁目1-43-507<br>
-					2-1-43 507, Daimyo,Chuo-ku,Fukuoka,810-0041
-				</p>
-				<p>T:+81-92-406-3290<br>F:+81-92-406-3291<br>E:info@starplatinum.jp.net</p>
-			</div>
-			<p class="mapIcon">
-				<a class="venobox vbox-item" data-type="iframe" href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1661.8247261616675!2d130.39528983770597!3d33.588449813802036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3541918672ab1493%3A0xfd082fa8bd8e0e33!2z56aP5bKh55yM56aP5bKh5biC5Lit5aSu5Yy65aSn5ZCN77yS5LiB55uu77yR4oiS77yU77yT!5e0!3m2!1sja!2sjp!4v1418190312031">
-					<img src="img/img1/icon_map.png" alt="マップ">
-				</a>
-			</p>
-			<div class="orangecss"></div>
-			</div>
-			<input type="checkbox" id="nav-trigger" class="nav-trigger" />
-			<label for="nav-trigger"></label>
-	';
+<!DOCTYPE HTML>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,height=device-height,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
 
-?>
+    <link rel="stylesheet" type="text/css" href="font-awesome-4.2.0/css/font-awesome.css">
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+<link rel="stylesheet" href="css/drawer.css"> <!-- Resource style -->
+	<link rel="stylesheet" href="css/venovox.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script type="text/javascript" src="js/venobox.min.js"></script>
+	<link href="http://fonts.googleapis.com/css?family=Roboto:400,700,300" rel="stylesheet" type="text/css">
+	<title>Star Platinum</title>
+</head>
+<body>
+	<div class="navigation">
+	<ul>
+	<li class="nav-item"><a href="#" class="menu-header"></a></li>
+	<li class="nav-item">
+ 		<a href="#" class="menu-links">>　会社概要</a>
+		<ul class="nav-item-sub">
+			<li class="menu-links"><a href="#">&nbsp;&nbsp;1</a></li>
+			<li class="menu-links"><a href="#">&nbsp;&nbsp;2</a></li>
+			<li class="menu-links"><a href="#">&nbsp;&nbsp;3</a></li>
+		</ul>
+	</li>
+	<li class="nav-item">
+		<a href="#" class="menu-links">>　制作実績</a>
+		<ul class="nav-item-sub">
+			<li class="menu-links"><a href="#">&nbsp;&nbsp;WEB関連</a></li>
+			<li class="menu-links"><a href="#">&nbsp;&nbsp;広告／印刷</a></li>
+			<li class="menu-links"><a href="#">&nbsp;&nbsp;教育／育成</a></li>
+			<li class="menu-links"><a href="#">&nbsp;&nbsp;イベント企画</a></li>
+			<li class="menu-links"><a href="#">&nbsp;&nbsp;マネージメント</a></li>
+		</ul>
+	</li>
+	<li class="nav-item"><a href="#" class="menu-links">>　お問い合わせ</a></li>
+	<li class="nav-item last-item"><a href="#" class="menu-copyright">© STAR PLATINUM INC. ALL RIGHTS RESERVED</a></li>
+	</ul>
+	<div class="contact">
+		<img src="img/img1/contact_us.png" alt="コンタクト">
+		<p>〒810-0041<br>
+			 福岡県福岡市中央区大名2丁目1-43-507<br>
+			2-1-43 507, Daimyo,Chuo-ku,Fukuoka,810-0041
+		</p>
+		<p>T:+81-92-406-3290<br>F:+81-92-406-3291<br>E:info@starplatinum.jp.net</p>
+	</div>
+	<p class="mapIcon">
+		<a class="venobox vbox-item" data-type="iframe" href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1661.8247261616675!2d130.39528983770597!3d33.588449813802036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3541918672ab1493%3A0xfd082fa8bd8e0e33!2z56aP5bKh55yM56aP5bKh5biC5Lit5aSu5Yy65aSn5ZCN77yS5LiB55uu77yR4oiS77yU77yT!5e0!3m2!1sja!2sjp!4v1418190312031">
+			<img src="img/img1/icon_map.png" alt="マップ">
+		</a>
+	</p>
+	<div class="orangecss"></div>
+	</div>
+	<input type="checkbox" id="nav-trigger" class="nav-trigger" />
+	<label for="nav-trigger"></label>
